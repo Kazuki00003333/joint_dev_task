c030f7ff28744434b11c524a3334fa103951f327
KAZUKI <k.kingfootball@gmail.com>
2020-04-09 17:18:51 +0900

    add_introduce

--- task.rb
+++ task.rb
@@ -124,8 +124,7 @@ def q14
   data = { name: "satou", age: 33, address: "saitama", hobby: "soccer", email: "hoge@fuga.com" }
 
   # 以下に回答を記載
-array = data.keys
-p array
+p data.keys
 end
 
 def q15
@@ -134,6 +133,8 @@ def q15
 
   # 以下に回答を記載
 
+p data1.key?(:age)? "OK":"NG"
+p data2.key?(:age)? "OK":"NG"
 end
 
 def q16
@@ -145,6 +146,9 @@ def q16
   ]
 
   # 以下に回答を記載
+users.each do |user|
+  puts "私の名前は#{user[:name]}です。年齢は#{user[:age]}何歳です。"
+
 
 end
 
